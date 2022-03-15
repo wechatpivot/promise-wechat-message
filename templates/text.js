f388649245f3254c968bfcf3ac7f543b12db65d6
hbrls <shuaizhexu@gmail.com>
2022-03-15 11:22:09 +0800

    !!!

---
+++ templates/text.js
@@ -0,0 +1,9 @@
+module.exports = function (from, to, content) {
+  return `
+    <ToUserName><![CDATA[${to}]]></ToUserName>
+    <FromUserName><![CDATA[${from}]]></FromUserName>
+    <CreateTime>${Math.floor(Date.now() / 1000)}</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[${content}]]></Content>
+  `;
+};
